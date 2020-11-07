30cb0c367bb81cecaaddec291b99bcc334b73ef0
Fiorella <you@example.com>
2020-11-07 15:43:45 -0500

    wuuu

--- DTO2/DTO_Receta.cs
+++ DTO2/DTO_Receta.cs
@@ -10,9 +10,8 @@ namespace DTO
         public string R_nombreReceta { get; set; }
         public int R_numeroPorcion { get; set; }
         public string R_descripcion { get; set; }
-        public Byte[] R_imagenReceta { get; set; }
+        public byte[] R_imagenReceta { get; set; }
         public int CR_idCategoriaReceta { get; set; }
-       
 
     }
 }
