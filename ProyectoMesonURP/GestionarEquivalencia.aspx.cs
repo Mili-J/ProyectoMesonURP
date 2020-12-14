5fc1cc8289076b2bbb1b6d97a3a9fd931e3dc755
Milagros <milagros_jesus98@hotmail.com>
2020-12-14 01:56:17 -0500

    cc GE

--- ProyectoMesonURP/GestionarEquivalencia.aspx.cs
+++ ProyectoMesonURP/GestionarEquivalencia.aspx.cs
@@ -11,14 +11,26 @@ namespace ProyectoMesonURP
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ListItem ddl1 = new ListItem("5", "5");
+            ddlp.Items.Insert(0, ddl1);
+            ListItem ddl2 = new ListItem("10", "10");
+            ddlp.Items.Insert(1, ddl2);
+            ListItem ddl3 = new ListItem("20", "20");
+            ddlp.Items.Insert(2, ddl3);
         }
 
         protected void btnAnadirEquivalencia_Click(object sender, EventArgs e)
         {
             Response.Redirect("AgregarEquivalencia.aspx");
         }
-
+        protected void ddlp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        protected void fnombreEq_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
         protected void btnEditarEquivalencia_Click(object sender, EventArgs e)
         {
 
