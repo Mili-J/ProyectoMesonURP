a24d1bb0413a15b31269fec80619257e5c3dc796
Milagros <milagros_jesus98@hotmail.com>
2021-01-03 19:07:38 -0500

    consultar equivalencia

---
+++ DTO2/DTO_Equivalencia_SP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO2
+{
+    public class DTO_Equivalencia_SP
+    {
+        public int I_idIngrediente { get; set; }
+        public string I_nombreIngrediente { get; set; }
+        public decimal E_cantidad { get; set; }
+        public int MXFC_idMedidaFCocina { get; set; }
+        public String M_nombreMedida { get; set; }
+        public String FCO_nombreFormatoCocina { get; set; }
+    }
+}
--- ProyectoMesonURP/GestionarEquivalencia.aspx.designer.cs
+++ ProyectoMesonURP/GestionarEquivalencia.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace ProyectoMesonURP
     {
 
         /// <summary>
+        /// Control PanelIngrediente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl PanelIngrediente;
+
+        /// <summary>
         /// Control ddlp.
         /// </summary>
         /// <remarks>
@@ -130,5 +139,32 @@ namespace ProyectoMesonURP
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvEquivalencia;
+
+        /// <summary>
+        /// Control hidden.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hidden;
+
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// Control HiddenField1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HiddenField1;
     }
 }
